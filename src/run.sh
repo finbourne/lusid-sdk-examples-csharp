68394558f7d6abd14956c362666a22dbf4a59715
matthew-boardman <matthew.boardman@finbourne.com>
2022-03-09 13:33:35 +0000

    CTECH-942 Github builds and docker files

---
+++ src/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -e
+
+dotnet restore
+dotnet build --configuration Release --no-restore
+dotnet LusidFeatureReporter/bin/Release/netcoreapp3.1/LusidFeatureReporter.dll -a Lusid.Sdk.Examples -n  Lusid.Sdk.Examples -o features.txt
+dotnet test -v n Lusid.Sdk.Tests
