219b4f8bfe94730054f97065456fc7050bff7211
matthew-boardman <matthew.boardman@finbourne.com>
2022-03-09 13:53:24 +0000

    CTECH-942 small fixes

--- src/run.sh
+++ src/run.sh
@@ -3,4 +3,4 @@
 dotnet restore
 dotnet build --configuration Release --no-restore
 dotnet LusidFeatureReporter/bin/Release/netcoreapp3.1/LusidFeatureReporter.dll -a Lusid.Sdk.Examples -n  Lusid.Sdk.Examples -o features.txt
-dotnet test -v n Lusid.Sdk.Tests
+dotnet test -v n Lusid.Sdk.Examples
